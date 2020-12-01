1194891c610cfc798facf5c94eac8c29eab955f8
William Keller <william@arda.net>
2020-12-01 00:27:42 -0700

    Docs for libraries

--- src/load.js
+++ src/load.js
@@ -4,6 +4,11 @@ const fs = require('fs')
 
 const DATA_DIR = path.resolve('./data')
 
+/**
+ * Loads a data file that is made up of lines of numbers, one value per line.
+ * @param {string} name - Name of the file to load (from the data directory)
+ * @return {array} The loaded numbers
+ */
 export function loadLinesToNumberArray(name) {
   return fs.readFileSync(path.join(DATA_DIR, name), 'utf8')
            .split('\n').map(x => parseInt(x))
