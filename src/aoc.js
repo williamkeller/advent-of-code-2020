907a218f3c81860d6f2dc7c9f78c661fc5cf5ca9
William Keller <william@arda.net>
2020-12-02 17:55:10 -0700

    cleanupt

--- src/aoc.js
+++ src/aoc.js
@@ -1,10 +1,18 @@
-// Data file loaders
 const path = require('path')
 const fs = require('fs')
 
-const DATA_DIR = path.resolve('./data')
 
-export function loadLinesToArray(name) {
+function dataDir() {
+  return path.resolve('./data')
+}
+
+/**
+ * Loads a data file that is made up of lines of text.
+ * @param {string} name - Name of the file to load (from the data directory)
+ * @param {string} dir - Data directory (usually only used during testing)
+ * @return {array} The loaded text strings
+ */
+export function loadLinesToArray(name, dir = dataDir()) {
   return fs.readFileSync(path.join(DATA_DIR, name), 'utf8').split('\n')
 }
 
@@ -12,13 +20,19 @@ export function loadLinesToArray(name) {
 /**
  * Loads a data file that is made up of lines of numbers, one value per line.
  * @param {string} name - Name of the file to load (from the data directory)
+ * @param {string} dir - Data directory (usually only used during testing)
  * @return {array} The loaded numbers
  */
-export function loadLinesToNumberArray(name) {
+export function loadLinesToNumberArray(name, dir = dataDir()) {
   return loadLinesToArray(name).map(x => parseInt(x))
 }
 
 
+/**
+ * Calls a callback for every two element permutation of the array
+ * @param {array} array - The array to permute
+ * @param {callback} callback - (object1, object2)
+ **/
 export function permute2(array, callback) {
   let len = array.length
   for(let i = 0; i < len - 1; i++) {
@@ -30,6 +44,11 @@ export function permute2(array, callback) {
 }
 
 
+/**
+ * Calls a callback for every three element permutation of the array
+ * @param {array} array - The array to permute
+ * @param {callback} callback - (object1, object2)
+ **/
 export function permute3(array, callback) {
   let len = array.length
   for(let i = 0; i < len - 2; i++) {
--- test/aoc.test.js
+++ test/aoc.test.js
@@ -1,5 +1,10 @@
 const aoc = require('../src/aoc')
 
+describe('loader functions', () => {
+  
+})
+
+
 describe('support functions', () => {
   test('permute2', () => {
     const array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
