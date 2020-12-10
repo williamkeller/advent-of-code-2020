b6d6bb25ca8bce6d85dae5a29ac61aab4033f4a5
William Keller <william@arda.net>
2020-12-09 23:18:34 -0700

    Day 10 part 2, ended up not as hard as I was expecting

--- src/day10.js
+++ src/day10.js
@@ -19,9 +19,6 @@ class SolverDay10 extends SolverBase {
     let data = this.loadData()
     let device = data[data.length - 1] + 3
 
-    console.log(data)
-    console.log(data.length)
-
     let one_count = 0
     let three_count = 0
 
@@ -52,7 +49,57 @@ class SolverDay10 extends SolverBase {
 
 
   puzzle2() {
+    let data = this.loadData()
+    data.unshift(0)
+    data.push(data[data.length - 1] + 3)
+    let runs = []
 
+    let index = 1
+    let inRun
+    let runStart = 1
+
+    inRun = (data[0] == 1)
+
+    console.log(data)
+
+    while(index < data.length) {
+      let v = data[index] - data[index - 1]
+
+      if(v == 1) {
+        if(!inRun) {
+          inRun = true
+          runStart = index
+        }
+      } else {
+         if(inRun) {
+           inRun = false
+           runs.push([runStart - 1, index])
+         }
+      }
+      index += 1
+    }
+
+    let total = 1
+    for(let run of runs) {
+      let len = run[1] - run[0]
+      switch(len) {
+        case 2:
+          break
+        case 3: 
+          total *= 2
+          break
+        case 4:
+          total *= 4
+          break
+        case 5:
+          total *= 7
+          break
+        default:
+          throw `unexpected value ${len}`
+      }
+    }
+
+    console.log(total)
   }
 }
 
